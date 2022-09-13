05ffe89291c7eaf70b09c79dc6d4bf99295e803b
Bobby Palko <bobby@palko.dev>
2022-09-12 21:43:03 -0400

    update some info for day of (#11)
    * bug: single person parties not receiving API response
    
    * show notes on not attending and show guest name for rsvp'd
    
    * preserve guest name on rsvp change
    
    * Bump mongoose from 6.3.4 to 6.4.6 (#9)
    
    Bumps [mongoose](https://github.com/Automattic/mongoose) from 6.3.4 to 6.4.6.
    - [Release notes](https://github.com/Automattic/mongoose/releases)
    - [Changelog](https://github.com/Automattic/mongoose/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/Automattic/mongoose/compare/6.3.4...6.4.6)
    
    ---
    updated-dependencies:
    - dependency-name: mongoose
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix +1 name not showing in text field
    
    * updated info with things to do
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- faqs.ts
+++ faqs.ts
@@ -1,14 +1,48 @@
 export const faqs = [
   {
     question: 'Getting There',
+    answer: `
+      <h3>Driving In</h3>
+      <div style="
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      justify-content: space-between;
+      gap: 5px;
+      ">
+        <p>
+        The entrance to the Farmers Market is not well marked; we will have it decorated so you can find it easily. If you're driving in, once you turn towards the Aldi's Supermarket, keep going! You're almost there!</p>
+        <div style="
+          background-color: var(--bgPink); 
+          display: inline-block; 
+          padding: 3rem 1.5rem;
+          border-radius: 0.5em;
+          border: solid 1px var(--black);
+          max-width: 7em;
+          max-height: 7em;
+          justify-self: center;
+        ">
+        <a href="https://www.google.com/maps/place/Ithaca+Farmers+Market/@42.4509999,-76.5114921,17z/data=!3m1!4b1!4m5!3m4!1s0x89d0817c483332a1:0x697348f602028b6c!8m2!3d42.450996!4d-76.5093034" target="_blank" style="color: var(--cream);">
+        <address>
+        545 3rd St<br>
+        Steamboat Landing<br>
+        Ithaca, NY 14850-3208
+        </address>
+        </a>
+        </div>
+      </div>
+      <h3>Shuttle Info</h3>
+      <p>If you're staying at one of the <a href="/stay">hotels we've partnered with</a>, we'll have a shuttle to transport you to and from the venue. The two hotels are on adjacent properties, and the shuttle will leave from the parking lot between them at 5:00pm, so please be ready outside by 4:50pm! It will depart the venue at 11:00pm to return to the hotels. We'll have the bus stocked with some goodie bags for the ride! Please note: the bus ride is about 45 minutes each way.</p>
+    `,
+  },
+  {
+    question: 'The Day Before',
     answer: `<div style="
     display: grid;
     grid-template-columns: 1fr 1fr;
     justify-content: space-between;
     gap: 5px;
     ">
-      <p>
-      The entrance to the Farmers Market is not well marked; we will have it decorated so you can find it easily. If you're driving in, once you turn towards the Aldi's Supermarket, keep going! You're almost there!</p>
+      <p>If you're in town the day before, come visit for a little pre-wedding celebration where we're staying! If you're able to come, bring your own chairs/blankets to sit out by the fire pit by the lake; we should be out there around 9:30pm. It is a quiet neighborhood, so please be respectful of volume!</p>
       <div style="
         background-color: var(--bgPink); 
         display: inline-block; 
@@ -19,27 +53,89 @@ export const faqs = [
         max-height: 7em;
         justify-self: center;
       ">
-      <a href="https://www.google.com/maps/place/Ithaca+Farmers+Market/@42.4509999,-76.5114921,17z/data=!3m1!4b1!4m5!3m4!1s0x89d0817c483332a1:0x697348f602028b6c!8m2!3d42.450996!4d-76.5093034" target="_blank" style="color: var(--cream);">
-      <address>
-      545 3rd St<br>
-      Steamboat Landing<br>
-      Ithaca, NY 14850-3208
-      </address>
-      </a>
+        <a href="https://www.google.com/maps/place/7916+County+Rd+153,+Interlaken,+NY+14847/@42.640616,-76.6990857,17z/data=!3m1!4b1!4m5!3m4!1s0x89d096daefd2079f:0x3b8bb4df5758ab67!8m2!3d42.640616!4d-76.696897" target="_blank" style="color: var(--cream);">
+          <address>
+            7916 County Road 153<br>
+            Interlaken, NY 14847
+          </address>
+        </a>
       </div>
     </div>`,
   },
   {
-    question: 'Shuttle Info',
-    answer: `<p>
-    If you're staying at one of the <a href="/stay">hotels we've partnered with</a>, we'll have a shuttle to transport you to and from the venue. The two hotels are on adjacent properties, and the shuttle will leave from the parking lot between them at 5:30pm. It will depart the venue at midnight to return to the hotels. We'll have the bus stocked with some goodie bags for the ride! Please note: the bus ride is about 45 minutes each way.
-    </p>`,
+    question: 'The Day Of',
+    answer: `
+    <div>
+      <h3>Things To Do</h3>
+      <p>If you have some time before the ceremony, here are some places we enjoy that you can check out:</p>
+      <ul>
+        <li>The Farmers Market - Head over to the venue before 3:00pm and peruse all the different local offerings in the stalls.</li>
+        <li>Taughannock Falls State Park - They say Ithaca is gorges, and this one is a short, easy walk from the parking lot.</li>
+        <li><a href="https://www.visitithaca.com/attractions/downtown-ithaca-commons" target="_blank">Downtown Ithaca Commons</a> - This four-block, pedestrian-only section of Downtown is home to many unique stores and Ithaca restaurants.</li>
+      </ul>
+    </div>
+    <div>
+      <h3>Shuttle Info</h3>
+      <p>If you're staying at one of the <a href="/stay">hotels we've partnered with</a>, we'll have a shuttle to transport you to and from the venue. The two hotels are on adjacent properties, and the shuttle will leave from the parking lot between them at 5:00pm, so please be ready outside by 4:50pm! It will depart the venue at 11:00pm to return to the hotels. We'll have the bus stocked with some goodie bags for the ride! Please note: the bus ride is about 45 minutes each way.</p>
+    </div>
+    <div>
+      <h3>Dress Code</h3>
+      <p>Formal? Semi-formal? Black Tie? We have a hard enough time figuring it out ourselves! Simply, we ask that you wear something nice and traditionally wedding appropriate. While you don't need a suit and tie, please leave the jeans at home.</p>
+    </div>
+    `,
   },
   {
-    question: 'Dress Code',
-    answer: `<p>
-    Formal? Semi-formal? Black Tie? We have a hard enough time figuring it out ourselves! Simply, we ask that you wear something nice and traditionally wedding appropriate. While you don't need a suit and tie, please leave the jeans at home.
-    </p>`,
+    question: 'The Day After',
+    answer: `<div style="
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      justify-content: space-between;
+      gap: 5px;
+    ">
+      <p>Join us for a little post celebration breakfast Sunday morning at 9:30am! We'll be serving bagels, coffee, tea, and mimosas 🍾 at Taughannock Falls State Park - or our rental home if it's raining.</p>
+      <div style="
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        justify-content: space-between;
+        justify-self: center;
+        gap: 5px;
+      ">
+        <div style="
+          background-color: var(--bgPink); 
+          display: inline-block; 
+          padding: 3rem 1.5rem;
+          border-radius: 0.5em;
+          border: solid 1px var(--black);
+          max-width: 7em;
+          max-height: 7em;
+          justify-self: center;
+        ">
+          <a href="https://www.google.com/maps/place/Tauughannock+falls+Gorge+trail+parking/@42.5412097,-76.605224,15.32z/data=!4m5!3m4!1s0x0:0x8ecdb1ca6528682e!8m2!3d42.5454172!4d-76.5993274" target="_blank" style="color: var(--cream);">
+            <address>
+              Taughannock Falls State Park
+            </address>
+          </a>
+        </div>
+        <div style="
+        background-color: var(--bgPink); 
+        display: inline-block; 
+        padding: 3rem 1.5rem;
+        border-radius: 0.5em;
+        border: solid 1px var(--black);
+        max-width: 7em;
+        max-height: 7em;
+        justify-self: center;
+        ">
+          <a href="https://www.google.com/maps/place/7916+County+Rd+153,+Interlaken,+NY+14847/@42.640616,-76.6990857,17z/data=!3m1!4b1!4m5!3m4!1s0x89d096daefd2079f:0x3b8bb4df5758ab67!8m2!3d42.640616!4d-76.696897" target="_blank" style="color: var(--cream);">
+            <address>
+              Rental Home<br>
+              7916 County Road 153<br>
+              Interlaken, NY 14847
+            </address>
+          </a>
+        </div>
+      </div>
+    </div>`,
   },
   {
     question: 'Gifts',
